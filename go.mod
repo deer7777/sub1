32c5e62896a3b0f4e6e43b11cb5a253190756c68
congxia wu <1398173038@qq.com>
2024-03-14 21:03:55 +0800

    2.0.0

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/deer7777/sub1
+module github.com/deer7777/sub1/v2
 
 go 1.21.1
 
-require github.com/deer7777/subsub/v2 v2.0.1
+require github.com/deer7777/subsubGP v1.0.0
--- subprint.go
+++ subprint.go
@@ -1,9 +1,11 @@
 package sub1
 
-import "fmt"
-import subsub "github.com/deer7777/subsub/v2"
+import (
+	"fmt"
+	"github.com/deer7777/subsubGP"
+)
 
 func Sub1print() {
 	fmt.Println("Sub1")
-	fmt.Println(subsub.Print())
+	fmt.Println(subsubGP.Print())
 }
